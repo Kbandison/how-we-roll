b7fc24a32c49dbd7113db7eadddf8d1d058b93af
Kevin Bandison <kbandison@gmail.com>
2022-11-01 12:02:32 -0400

    added mode

--- main.js
+++ main.js
@@ -39,6 +39,11 @@ let medianDouble = document.querySelector('#double-d6-rolls-median');
 let medianD12 = document.querySelector('#d12-rolls-median');
 let medianD20 = document.querySelector('#d20-rolls-median');
 
+let modeD6 = document.querySelector('#d6-rolls-mode');
+let modeDouble = document.querySelector('#double-d6-rolls-mode');
+let modeD12 = document.querySelector('#d12-rolls-mode');
+let modeD20 = document.querySelector('#d20-rolls-mode');
+
 
 /*******************
  * EVENT LISTENERS *
@@ -83,19 +88,27 @@ let reset = () => {
     doubleSixes = [];
     twelves = [];
     twenties = [];
+
     imageD6.src = 'images/start/d6.png';
     imageDouble1.src = 'images/start/d6.png';
     imageDouble2.src = 'images/start/d6.png';
     imageD12.src = 'images/start/d12.jpeg';
     imageD20.src = 'images/start/d20.jpg';
+
     meanD6.innerText = 'N/A';
     meanDouble.innerText = 'N/A';
     meanD12.innerText = 'N/A';
     meanD20.innerText = 'N/A';
+
     medianD6.innerText = 'N/A';
     medianDouble.innerText = 'N/A';
     medianD12.innerText = 'N/A';
     medianD20.innerText = 'N/A';
+
+    modeD6.innerText = 'N/A';
+    modeDouble.innerText = 'N/A';
+    modeD12.innerText = 'N/A';
+    modeD20.innerText = 'N/A';
 }
 
 reset();
@@ -111,21 +124,24 @@ let d6RollFunc = () => {
     sixes.push(result);
     meanD6.innerText = getMean(sixes);
     medianD6.innerText = getMedian(sixes);
+    modeD6.innerText = getMode(sixes);
 }
 let double1RollFunc = () => {
     let result = getRandomNumber(6);
     imageDouble1.src = `images/d6/${result}.png`;
-    doubleSixes.push(result);
-    meanDouble.innerText = getMean(doubleSixes);
-    medianDouble.innerText = getMedian(doubleSixes);
-}
-
-let double2RollFunc = () => {
-    let result = getRandomNumber(6);
     imageDouble2.src = `images/d6/${result}.png`;
     doubleSixes.push(result);
     meanDouble.innerText = getMean(doubleSixes);
     medianDouble.innerText = getMedian(doubleSixes);
+    modeDouble.innerText = getMode(doubleSixes);
+}
+
+let double2RollFunc = () => {
+    let result = getRandomNumber(6);
+    doubleSixes.push(result);
+    meanDouble.innerText = getMean(doubleSixes);
+    medianDouble.innerText = getMedian(doubleSixes);
+    modeDouble.innerText = getMode(doubleSixes);
 }
 
 let d12RollFunc = () => {
@@ -134,14 +150,16 @@ let d12RollFunc = () => {
     twelves.push(result);
     meanD12.innerText = getMean(twelves);
     medianD12.innerText = getMedian(twelves);
+    modeD12.innerText = getMode(twelves);
 }
 
 let d20RollFunc = () => {
-    let result = getRandomNumber(6);
+    let result = getRandomNumber(20);
     imageD20.src = `images/numbers/${result}.png`;
     twenties.push(result);
     meanD20.innerText = getMean(twenties);
     medianD20.innerText = getMedian(twenties);
+    modeD20.innerText = getMode(twenties);
 }
 
 
@@ -165,15 +183,37 @@ let getMedian = arr => {
 
     sixes.sort();
     doubleSixes.sort();
-    twelves.sort();
-    twenties.sort();
+    twelves.sort((a, b) => a - b);      //comparison function (if a < b, b is placed before a)
+    twenties.sort((a, b) => a - b);
 
     for (let i = 0; i < arr.length; i++) {
-        if (arr.length % 2 === 0){
+        if (arr.length % 2 === 0) {
             return total += (arr[mid] + arr[mid - 1]) / 2;
-        } else if (arr.length % 2 != 0){
+        } else if (arr.length % 2 != 0) {
             return total += arr[mid];
-        } 
+        }
     }
+}
 
+let getMode = arr => {
+    let obj = {};
+    let highest = 0;
+    let count = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        let num = arr[i];               //num = key                                        
+                                        //obj[num] = value
+
+        if (!obj[num]) {                //if the key does not exist
+            obj[num] = 1;               //add it to the obj and set value to 1
+        } else {
+            obj[num]++;                 //increment the value by 1
+        }
+
+        if (count < obj[num]) {         //if count is less than the value in obj
+            highest = num;              //change the value of highest to the actual number
+            count = obj[num];           //change count to whatever the value in the obj is
+        }
+    }
+    return Number(highest);
 }
